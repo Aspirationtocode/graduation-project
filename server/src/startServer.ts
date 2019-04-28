39ecacb17120d5b27f6b5079108978a84bdbdd14
Aspirationtocode <aspiretocode@gmail.com>
2019-04-28 22:54:09 +0300

    feat: add travis

--- server/src/startServer.ts
+++ server/src/startServer.ts
@@ -3,23 +3,11 @@ import { serverConfig } from "../configs/server/config";
 import bodyParser = require("body-parser");
 import { buildSchema } from "type-graphql";
 import { UserResolver } from "./models/user/resolver";
-import * as path from "path";
 import * as graphqlHTTP from "express-graphql";
-import { paths } from "../../common/paths";
 
 const app = express();
 
 app.use(bodyParser.json());
-
-app.use(express.static(paths.frontend.DIST));
-
-app.get("/", (request, response) => {
-  response.sendFile(paths.frontend.INDEX_HTML);
-});
-
-app.get("/r", (request, response) => {
-  response.send("Hello");
-});
 
 export function startServer() {
   buildSchema({
