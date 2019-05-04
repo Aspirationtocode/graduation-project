4559b82be7f19e65a253aebc043cd8fbb1d20e5c
Aspirationtocode <aspiretocode@gmail.com>
2019-05-04 22:06:18 +0300

    fix

--- frontend/config/environment.ts
+++ frontend/config/environment.ts
@@ -9,6 +9,8 @@ export interface PROCESS_ENV {
 
 const env: PROCESS_ENV = process.env;
 
+console.log(env.mode, env.mode === "production");
+
 export module Environment {
   export const isProd = env.mode === "production";
   export const isDev = env.mode === "development";
